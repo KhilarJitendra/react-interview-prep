79b6ad6023f134d382aa3161b2cd5804f7160444
Jitendra <jitendra.khilar@akqa.com>
2023-11-22 20:35:54 +0530

    basics added

--- components/counters.tsx
+++ components/counters.tsx
@@ -7,8 +7,19 @@ function MyButton() {
   const [count, setCount] = useState(0)
 
   function handleClick() {
+    const vir_dom = <h1>This is a Button (virtual)</h1>
+    const real_dom = document.createElement('h1');
+
+    real_dom.innerText = 'This is a Button (real)'
+
+    console.log('vir_dom',vir_dom )
+    console.dir(real_dom)
     setCount(count + 1)
   }
+
+
+  
+
 
   return (
     <div>
